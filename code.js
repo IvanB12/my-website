57b1ee5571fac2ed5581ea6a842065a19b02f803
Ivan <ivanbozhko777@gmail.com>
2020-10-14 17:44:28 +0300

    Mobile version is ready. Added SEO part

--- code.js
+++ code.js
@@ -1,47 +1,48 @@
 
-var presentations = Array.from(document.querySelectorAll('.overlay'))
+var presentations = document.querySelectorAll('.overlay')
 var buttons = document.querySelectorAll('.button-projects-on')
+var hamburger = document.querySelector('.hamburger');
+var hamburgerMenu = document.querySelector('#mobile-menu')
+var links = document.querySelectorAll('a')
+var innerLinks = document.querySelectorAll('.smoothscroll')
+links.forEach(item => item.target = '_blank')
+innerLinks.forEach(item => item.target = '_self')
 //var buttons2 = document.querySelectorAll('.button-projects-off')
-console.log(buttons)
+//console.log(buttons)
+
+//buttons.forEach(item => item.innerHTML = 'Show info')
 buttons.forEach(item => {
     item.addEventListener('click', event => {
         var itemTarg = event.target.name
-        //console.log(itemTarg)
-
+        buttons[itemTarg].innerHTML.includes('Show') ? buttons[itemTarg].innerHTML = 'Hide info' : buttons[itemTarg].innerHTML = 'Show info';
         presentations[itemTarg].classList.toggle("visible");
+        console.log(buttons[itemTarg].innerHTML)
     })
 })
+
+hamburger.addEventListener('click', toggle)
+// hamburger.className === "is-active" ? hamburgerMenu.style.display = 'none' : hamburgerMenu.style.display = 'inline-block'
+function toggle() {
+    hamburger.classList.toggle("is-active");
+    if (hamburgerMenu.style.display === 'none') {
+        console.log(hamburgerMenu.style.display)
+        hamburgerMenu.style.display = 'inline-block'
+        console.log(hamburgerMenu.style.display)
+    }
+    else {
+        console.log(hamburgerMenu.style.display + ' from else block')
+        hamburgerMenu.style.display = 'none'
+        console.log(hamburgerMenu.style.display + ' from else block')
+    }
+
+}
+
 
 
 //https:www.codewars.com/users/IvanB12
 
 
 
-
-
-/*
-buttons2.forEach(item => {
-    item.addEventListener('click', event => {
-        var itemTarg = event.target.name
-        // presentations[itemTarg].classList.toggle("visible", true);
-        //presentations[itemTarg].classList.toggle("overlay");
-        presentations[itemTarg].classList.toggle("visible");
-
-
-    })
-})
-*/
-
-
-/*var presentations = document.querySelectorAll('.overlay')
-console.log(presentations)
-
-var buttons = document.querySelectorAll('.button-projects-on');
-var buttons2 = document.querySelectorAll('.button-projects-off')
-buttons.forEach(item => item.addEventListener('click', function (event) {
-    var targetItem = event.target.name;
-    console.log(targetItem)
-}))*/
 
 
 
@@ -63,3 +64,6 @@ animation going out of the site and cause white strip on the right side.
 pay attention if header is bigger (not nessesary if hamburger)*/
 
 /*hamburger to header and probably */
+
+/* 1) fixed changing button sign from "show info" to "hide info", on click
+2) changed size of profile-pic for mobile devices*/
